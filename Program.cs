754bc9e864715958ed40bca4d3f8406507e59d02
Rafael Elias <rafaelelias@outlook.com>
2025-08-17 02:32:42 -0300

    ajuste

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
